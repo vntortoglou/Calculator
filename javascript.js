3892048f2d43f0f1aed789b4c25fb34e69c7c8dc
vntortoglou <vntortoglou@gmail.com>
2024-06-13 14:32:56 +0300

    Finished with all the sytles of the calculator. I'm open to change them again in the end

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const btn00 = document.getElementById('btn00');
+const btn0 = document.getElementById('btn0');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+const btn7 = document.getElementById('btn7');
+const btn8 = document.getElementById('btn8');
+const btn9 = document.getElementById('btn9');
+
+
+
+
+btn1.addEventListener('click', function(){
+    console.log("Button1")
+});
+
+btn2.addEventListener('click', function(){
+    console.log("Button2")
+});
